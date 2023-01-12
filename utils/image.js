9e8a271666a3cf7f125ea17e8d1331f4b90ee92d
Justin Jerez <76711971+justinjerezz@users.noreply.github.com>
2023-01-12 13:14:59 +0100

    Console LOg

--- utils/image.js
+++ utils/image.js
@@ -1,6 +1,8 @@
 function getFilePath(file){
     const filePath = file.path;
+    console.log("FILE PATH",filePath);
     const fileSplit= filePath.split("\\");
+    console.log("FILE SPLIT",fileSplit);
 
     return `${fileSplit[1]}/${fileSplit[2]}`;
 
